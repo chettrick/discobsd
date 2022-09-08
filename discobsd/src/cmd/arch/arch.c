9c3a48436576a58cdc214d1e0a2bded466c5f6db
Chris Hettrick <git@structfoo.com>
2022-09-08 14:25:59 -0700

    src/cmd: Import arch(1) from OpenBSD. 2-clause BSD-style license.
    arch(1) has two names: arch and machine.
    machine outputs to stdout the MACHINE define ('stm32' on DiscoBSD).
    arch -s outputs to stdout the MACHINE_ARCH define ('arm' on DiscoBSD).
    
    arch (without the -s option) outputs '2.11BSD.arm' to stdout.
    This is not preferred, as the dot in '2.11BSD' could cause issues in
    shell scripts, since the latter dot is the name separator between the
    operating system and the architecture. A future commit may replace
    '2.11BSD' with either 'DiscoBSD' or 'RetroBSD', as the case may be.
    
    Added arch(1) and its manpage to the rootfs.manifest* files,
    with links for machine(1).
    
    'machine' is analogous with 'uname -m'.
    'arch -s' is analogous with 'uname -p'.

---
+++ discobsd/src/cmd/arch/arch.c
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 1994 SigmaSoft, Th. Lockert <tholo@sigmasoft.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/param.h>	/* MACHINE MACHINE_ARCH */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static void usage(void);
+
+static int machine;
+
+int
+main(int argc, char *argv[])
+{
+	extern const char *__progname;
+	int short_form = 0, c;
+	char *arch, *opts;
+
+	machine = strcmp(__progname, "machine") == 0;
+	if (machine) {
+		arch = MACHINE;
+		opts = "a";
+		short_form = 1;
+	} else {
+		arch = MACHINE_ARCH;
+		opts = "ks";
+	}
+	while ((c = getopt(argc, argv, opts)) != -1) {
+		switch (c) {
+		case 'a':
+			arch = MACHINE_ARCH;
+			break;
+		case 'k':
+			arch = MACHINE;
+			break;
+		case 's':
+			short_form = 1;
+			break;
+		default:
+			usage();
+		}
+	}
+	if (optind != argc)
+		usage();
+
+	printf("%s%s\n", short_form ? "" : "2.11BSD.", arch);
+	return (0);
+}
+
+static void
+usage(void)
+{
+	if (machine)
+		fprintf(stderr, "usage: machine [-a]\n");
+	else
+		fprintf(stderr, "usage: arch [-ks]\n");
+	exit(1);
+}

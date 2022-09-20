b8a9cb8fb717ef008cbaad9ce6191fbab6d0db03
Chris Hettrick <git@structfoo.com>
2022-09-19 17:43:37 -0700

    sys/stm32: Refactor kernel compile options in Makefile.
    Another big overhaul of the kconfig-generated kernel Makefile build.
    
    Keep compiler tools and the paths to them out of the kernel Makefile
    and put them in gcc-config.mk where they belong.
    This was made possible due to grouping compiler options and flags by
    their need and purpose. Inspiration came from OpenBSD kernel Makefiles.
    
    No binary change in the kernel build with arm-none-eabi gcc tools.
    
    Formatting changes in Makefile.kconf (mostly tabs, =, and {} fixes).
    
    Regen f469idisco/Makefile. Clang is not too far off.

--- discobsd/sys/stm32/gcc-config.mk
+++ discobsd/sys/stm32/gcc-config.mk
@@ -9,6 +9,14 @@
 # conflicts with the standard version of gdb from the ports tree,
 # so choose only one to install on your system at a time.
 ifndef ARM_GCC_PREFIX
+    _AS		= gcc
+    _CC		= gcc
+    _CPP	= cpp
+    _LD		= ld
+    _SIZE	= size
+    _OBJCOPY	= objcopy
+    _OBJDUMP	= objdump
+
     ifeq (/usr/local/bin/arm-none-eabi-gcc,$(wildcard /usr/local/bin/arm-none-eabi-gcc))
         ARM_GCC_PREFIX = /usr/local/bin/arm-none-eabi-
     endif
@@ -18,3 +26,11 @@ ifndef ARM_GCC_PREFIX
         ARM_GCC_PREFIX = /usr/bin/arm-none-eabi-
     endif
 endif
+
+AS=		${ARM_GCC_PREFIX}${_AS}
+CC=		${ARM_GCC_PREFIX}${_CC}
+CPP=		${ARM_GCC_PREFIX}${_CPP}
+LD=		${ARM_GCC_PREFIX}${_LD}
+SIZE=		${ARM_GCC_PREFIX}${_SIZE}
+OBJCOPY=	${ARM_GCC_PREFIX}${_OBJCOPY}
+OBJDUMP=	${ARM_GCC_PREFIX}${_OBJDUMP}

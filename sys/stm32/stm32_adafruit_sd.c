d588a9b80cd950a9a652b6c64a2417f2712b392e
Chris Hettrick <git@structfoo.com>
2022-11-06 19:31:39 -0800

    sys/stm32: Bug fix to read and write SDHC cards.
    Bug in BSP_SD_ReadBlocks() and BSP_SD_WriteBlocks() in the handling
    of the read or write address in SDHC card commands when reading or
    writing more than one block.
    
    The fix is to not assume SDHC cards count incremental addresses by
    an SD blocksize (512 bytes). SDHC cards count by 1.
    
    Bug fix came from the V3.0.3 Adafruit SD library, on April 30, 2018,
    which was included in STM32Cube_FW_F4_V1.25.0 Released 2020-02-12.

--- sys/stm32/stm32_adafruit_sd.c
+++ sys/stm32/stm32_adafruit_sd.c
@@ -2,8 +2,8 @@
   ******************************************************************************
   * @file    stm32_adafruit_sd.c
   * @author  MCD Application Team
-  * @version V3.0.0
-  * @date    23-December-2016
+  * @version V3.0.3
+  * @date    30-April-2018
   * @brief   This file provides a set of functions needed to manage the SD card
   *          mounted on the Adafruit 1.8" TFT LCD shield (reference ID 802),
   *          that is used with the STM32 Nucleo board through SPI interface.
@@ -343,6 +343,7 @@ uint8_t BSP_SD_GetCardInfo(SD_CardInfo *pCardInfo)
 uint8_t BSP_SD_ReadBlocks(uint32_t *pData, uint32_t ReadAddr, uint32_t NumOfBlocks, uint32_t Timeout)
 {
   uint32_t offset = 0;
+  uint32_t addr;
   uint8_t retr = BSP_SD_ERROR;
   uint8_t *ptr = NULL;
   SD_CmdAnswer_typedef response;
@@ -365,12 +366,15 @@ uint8_t BSP_SD_ReadBlocks(uint32_t *pData, uint32_t ReadAddr, uint32_t NumOfBloc
   }
   memset(ptr, SD_DUMMY_BYTE, sizeof(uint8_t)*BlockSize);
 
+  /* Initialize the address */
+  addr = (ReadAddr * ((flag_SDHC == 1) ? 1 : BlockSize));
+
   /* Data transfer */
   while (NumOfBlocks--)
   {
     /* Send CMD17 (SD_CMD_READ_SINGLE_BLOCK) to read one block */
     /* Check if the SD acknowledged the read block command: R1 response (0x00: no errors) */
-    response = SD_SendCmd(SD_CMD_READ_SINGLE_BLOCK, (ReadAddr + offset) * (flag_SDHC == 1 ? 1: BlockSize), 0xFF, SD_ANSWER_R1_EXPECTED);
+    response = SD_SendCmd(SD_CMD_READ_SINGLE_BLOCK, addr, 0xFF, SD_ANSWER_R1_EXPECTED);
     if ( response.r1 != SD_R1_NO_ERROR)
     {
       goto error;
@@ -384,6 +388,8 @@ uint8_t BSP_SD_ReadBlocks(uint32_t *pData, uint32_t ReadAddr, uint32_t NumOfBloc
 
       /* Set next read address*/
       offset += BlockSize;
+      addr = ((flag_SDHC == 1) ? (addr + 1) : (addr + BlockSize));
+
       /* get CRC bytes (not really needed by us, but required by SD) */
       SD_IO_WriteByte(SD_DUMMY_BYTE);
       SD_IO_WriteByte(SD_DUMMY_BYTE);
@@ -422,6 +428,7 @@ error:
 uint8_t BSP_SD_WriteBlocks(uint32_t *pData, uint32_t WriteAddr, uint32_t NumOfBlocks, uint32_t Timeout)
 {
   uint32_t offset = 0;
+  uint32_t addr;
   uint8_t retr = BSP_SD_ERROR;
   uint8_t *ptr = NULL;
   SD_CmdAnswer_typedef response;
@@ -443,12 +450,15 @@ uint8_t BSP_SD_WriteBlocks(uint32_t *pData, uint32_t WriteAddr, uint32_t NumOfBl
     goto error;
   }
 
+  /* Initialize the address */
+  addr = (WriteAddr * ((flag_SDHC == 1) ? 1 : BlockSize));
+
   /* Data transfer */
   while (NumOfBlocks--)
   {
     /* Send CMD24 (SD_CMD_WRITE_SINGLE_BLOCK) to write blocks  and
        Check if the SD acknowledged the write block command: R1 response (0x00: no errors) */
-    response = SD_SendCmd(SD_CMD_WRITE_SINGLE_BLOCK, (WriteAddr + offset) * (flag_SDHC == 1 ? 1 : BlockSize), 0xFF, SD_ANSWER_R1_EXPECTED);
+    response = SD_SendCmd(SD_CMD_WRITE_SINGLE_BLOCK, addr, 0xFF, SD_ANSWER_R1_EXPECTED);
     if (response.r1 != SD_R1_NO_ERROR)
     {
       goto error;
@@ -466,6 +476,7 @@ uint8_t BSP_SD_WriteBlocks(uint32_t *pData, uint32_t WriteAddr, uint32_t NumOfBl
 
     /* Set next write address */
     offset += BlockSize;
+    addr = ((flag_SDHC == 1) ? (addr + 1) : (addr + BlockSize));
 
     /* Put CRC bytes (not really needed by us, but required by SD) */
     SD_IO_WriteByte(SD_DUMMY_BYTE);

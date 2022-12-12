chettrick/discobsd

commit e5ee8aab51cb41710c6db5fe49202854840ee02e

sys/stm32: LEDs, button defined only in Config for F469I.
LEDs and buttons are now able to be fully defined through declaring
'signal' lines in Config, such as 'signal "LED1" pin PG6' which will
generate the line 'PARAM += -DLED1_GPIO_PORT=GPIOG -DLED1_PIN=6' in
the kernel Makefile, when generated by tools/kconfig/kconfig.
Currently, an options such as 'options "BUTTON_USER_PRESSED=1"' is
required to configure for either active-high or active-low buttons.
'invert' will be used in a future commit to remove this present need.

Unused aliases and previously defined LED and button defines are
removed from the BSP (board support package) for the STM32F469IDISCO.

Minor costmetic cleanups.
Regen kernel Makefile.

sys/stm32/f469idisco/Makefile
@@ -4,6 +4,12 @@ PARAM += -DUART3_ENABLED
 PARAM += -DUART6_ENABLED
 PARAM += -DSDIO_ENABLED
 PARAM += -DSD_ENABLED
+PARAM += -DUSER_BUTTON_GPIO_PORT=GPIOA -DUSER_BUTTON_PIN=0
+PARAM += -DLED4_GPIO_PORT=GPIOK -DLED4_PIN=3
+PARAM += -DLED3_GPIO_PORT=GPIOD -DLED3_PIN=5
+PARAM += -DLED2_GPIO_PORT=GPIOD -DLED2_PIN=4
+PARAM += -DLED1_GPIO_PORT=GPIOG -DLED1_PIN=6
+PARAM += -DBUTTON_USER_PRESSED=1
 PARAM += -DNSD=1
 PARAM += -DSD_MHZ=10
 PARAM += -DCONS_MINOR=2
@@ -11,7 +17,6 @@ PARAM += -DCONS_MAJOR=UART_MAJOR
 PARAM += -DUART_BAUD=115200
 PARAM += -DHALTREBOOT
 PARAM += -DMAXMEM=112*1024
-PARAM += -DBUS_DIV=1
 PARAM += -DBUS_KHZ=90000
 PARAM += -DCPU_KHZ=180000
 LDSCRIPT = "f469idisco/STM32F469NIHX.ld"

sys/stm32/f469idisco/Config
@@ -19,9 +19,7 @@ ldscript        "f469idisco/STM32F469NIHX.ld" # Linker script
 # Standard system options
 options         "CPU_KHZ=180000"            # Oscillator frequency of CPU core
 options         "BUS_KHZ=90000"             # Frequency of peripheral bus
-options         "BUS_DIV=1"                 # Bus clock divisor 1/2/4/8
 options         "MAXMEM=112*1024"           # Max core per process allowed
-#options         "USE_FULL_LL_DRIVER=1"      # Use full low-level driver
 options         "HALTREBOOT"                # If halted, press key to reboot
 
 # Root filesystem at /dev/sd0a, swap at /dev/sd0b
@@ -53,3 +51,13 @@ device          sd0     at sdio0            # SD card uses SDIO port
 #options         "SPI_SD_ENABLED"            # Enable SPI-based SD card
 options         "SD_MHZ=10"                 # speed 10 MHz
 options         "NSD=1"                     # One disk on SDIO.
+
+# Four LEDs are connected to MCU on PG6, PD4, PD5, PK3
+signal          "LED1" pin PG6
+signal          "LED2" pin PD4
+signal          "LED3" pin PD5
+signal          "LED4" pin PK3
+
+# User Button is connected to MCU on PA0
+signal          "USER_BUTTON" pin PA0
+options         "BUTTON_USER_PRESSED=1"

sys/stm32/stm32469i_discovery.h
@@ -38,7 +38,6 @@
 
 #ifdef F469IDISCO                       /* Kernel Config 'board' definition. */
 
-/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32469I_DISCOVERY_H
 #define __STM32469I_DISCOVERY_H
 
@@ -46,67 +45,31 @@
  extern "C" {
 #endif
 
-/* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx_ll_bus.h"
 #include "stm32f4xx_ll_gpio.h"
 
-/** @brief Led_TypeDef
-  *  STM32469I_Discovery board leds definitions.
-  */
 typedef enum
 {
- LED1 = 0,
- LED_GREEN = LED1,
- LED2 = 1,
- LED_ORANGE = LED2,
- LED3 = 2,
- LED_RED = LED3,
- LED4 = 3,
- LED_BLUE = LED4
-
+  LED1 = 0,
+  LED_GREEN = LED1,
+  LED2 = 1,
+  LED_ORANGE = LED2,
+  LED3 = 2,
+  LED_RED = LED3,
+  LED4 = 3,
+  LED_BLUE = LED4,
 } Led_TypeDef;
 
-/** @brief Button_TypeDef
-  *  STM32469I_Discovery board Buttons definitions.
-  */
 typedef enum
 {
   BUTTON_USER = 0,
-  /* Alias */
-  BUTTON_WAKEUP = BUTTON_USER
 } Button_TypeDef;
 
-/** @brief ButtonMode_TypeDef
-  *  STM32469I_Discovery board Buttons Modes definitions.
-  */
 typedef enum
 {
-  BUTTON_MODE_GPIO = 0
+  BUTTON_MODE_GPIO = 0,
 } ButtonMode_TypeDef;
-
-/* 4 Leds are connected to MCU directly on PG6, PD4, PD5, PK3 */
-#define LED1_PIN                        ((uint32_t)LL_GPIO_PIN_6)
-#define LED2_PIN                        ((uint32_t)LL_GPIO_PIN_4)
-#define LED3_PIN                        ((uint32_t)LL_GPIO_PIN_5)
-#define LED4_PIN                        ((uint32_t)LL_GPIO_PIN_3)
-
-#define LED1_GPIO_PORT                  ((GPIO_TypeDef *)GPIOG)
-#define LED2_GPIO_PORT                  ((GPIO_TypeDef *)GPIOD)
-#define LED3_GPIO_PORT                  ((GPIO_TypeDef *)GPIOD)
-#define LED4_GPIO_PORT                  ((GPIO_TypeDef *)GPIOK)
-
-/**
-  * @brief User push-button
-  */
-#define BUTTON_USER_PRESSED             ((uint32_t)1)
-
-#define USER_BUTTON_PIN                 ((uint32_t)LL_GPIO_PIN_0)
-#define USER_BUTTON_GPIO_PORT           ((GPIO_TypeDef *)GPIOA)
-
-/* Define the Wakeup button as an alias of the USER button */
-#define WAKEUP_BUTTON_PIN               USER_BUTTON_PIN
-#define WAKEUP_BUTTON_GPIO_PORT         USER_BUTTON_GPIO_PORT
 
 uint32_t         BSP_GetVersion(void);
 void             BSP_LED_Init(Led_TypeDef Led);

commit 6e08630f98cbd0540882d3510911b16653b075d4

sys/stm32: Bugfix for kernel Config GPIO pin defines.
Bug introduced in commit e5ee8aab51cb41710c6db5fe49202854840ee02e
for GPIO pins. The pin parameter for LL_GPIO_* functions requires
a pin bit position, not just a pin number. This caused button and
LED GPIOs to be incorrectly configured and to not work as intented.

Correct this bug by modifying kconfig to generate an 'LL_GPIO_PIN_'
prefix to the Config'd pin number, such as 'LL_GPIO_PIN_6'.

A configuration line such as 'signal "LED1" pin PG6' in Config
generates 'PARAM += -DLED1_GPIO_PORT=GPIOG -DLED1_PIN=LL_GPIO_PIN_6'
in the kernel Makefile, when generated by tools/kconfig/kconfig.

Regen f469idisco kernel Makefile.

sys/stm32/f469idisco/Makefile
@@ -4,11 +4,11 @@ PARAM += -DUART3_ENABLED
 PARAM += -DUART6_ENABLED
 PARAM += -DSDIO_ENABLED
 PARAM += -DSD_ENABLED
-PARAM += -DUSER_BUTTON_GPIO_PORT=GPIOA -DUSER_BUTTON_PIN=0
-PARAM += -DLED4_GPIO_PORT=GPIOK -DLED4_PIN=3
-PARAM += -DLED3_GPIO_PORT=GPIOD -DLED3_PIN=5
-PARAM += -DLED2_GPIO_PORT=GPIOD -DLED2_PIN=4
-PARAM += -DLED1_GPIO_PORT=GPIOG -DLED1_PIN=6
+PARAM += -DUSER_BUTTON_GPIO_PORT=GPIOA -DUSER_BUTTON_PIN=LL_GPIO_PIN_0
+PARAM += -DLED4_GPIO_PORT=GPIOK -DLED4_PIN=LL_GPIO_PIN_3
+PARAM += -DLED3_GPIO_PORT=GPIOD -DLED3_PIN=LL_GPIO_PIN_5
+PARAM += -DLED2_GPIO_PORT=GPIOD -DLED2_PIN=LL_GPIO_PIN_4
+PARAM += -DLED1_GPIO_PORT=GPIOG -DLED1_PIN=LL_GPIO_PIN_6
 PARAM += -DBUTTON_USER_PRESSED=1
 PARAM += -DNSD=1
 PARAM += -DSD_MHZ=10

tools/kconfig/mkmakefile.c
@@ -475,7 +475,7 @@ void makefile()
                     sig->sig_name, 'A'+port-1, bit);
                 exit(1);
             }
-            fprintf(ofp, "PARAM += -D%s_GPIO_PORT=GPIO%c -D%s_PIN=%d",
+            fprintf(ofp, "PARAM += -D%s_GPIO_PORT=GPIO%c -D%s_PIN=LL_GPIO_PIN_%d",
                 sig->sig_name, 'A'+port-1, sig->sig_name, bit);
             break;
 
c2523dddb1a21484b41c867b84e83f40c2e2e64b
Chris Hettrick <git@structfoo.com>
2023-04-11 14:15:23 -0700

    sys/stm32: Portable sys/stm32/gcc-config.mk makefile.
    A few tricks to remove the gmake-isms from gcc-config.mk and make
    it portable with gmake and the various BSD makes.
    
    Determine the host operating system name to select the path to the
    GCC arm-none-eabi compiler tools, and fail with /does/not/exist.
    
    != runs the shell commands and saves the output to ARM_GCC_PREFIX.
    
    On the command line a different tool prefix can be used like:
      $ make ARM_GCC_PREFIX="/path/to/my/arm-none-eabi"
    
    Instructions on installing the toolchain on OpenBSD are available
    from tools/openbsd/README.md in the repository.

--- sys/stm32/gcc-config.mk
+++ sys/stm32/gcc-config.mk
@@ -1,34 +1,23 @@
-# Generic ARM toolchain on OpenBSD
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# You can install the arm-none-eabi package, Linaro version, via:
-#   $ doas pkg_add arm-none-eabi-gcc-linaro
-# This also installs binutils and newlib for arm-none-eabi target.
-# You can install gdb for arm-none-eabi via:
-#   $ doas pkg_add arm-none-eabi-gdb
-# Note that the arm-none-eabi version of gdb currently (as of OpenBSD 6.6)
-# conflicts with the standard version of gdb from the ports tree,
-# so choose only one to install on your system at a time.
-ifndef ARM_GCC_PREFIX
-    _AS		= gcc
-    _CC		= gcc
-    _CPP	= cpp
-    _LD		= ld
-    _SIZE	= size
-    _OBJCOPY	= objcopy
-    _OBJDUMP	= objdump
+# GCC arm-none-eabi toolchain on OpenBSD and Linux
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    ifeq (/usr/local/bin/arm-none-eabi-gcc,$(wildcard /usr/local/bin/arm-none-eabi-gcc))
-        ARM_GCC_PREFIX = /usr/local/bin/arm-none-eabi-
-    endif
-# Generic ARM toolchain on Linux
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    ifeq (/usr/bin/arm-none-eabi-gcc,$(wildcard /usr/bin/arm-none-eabi-gcc))
-        ARM_GCC_PREFIX = /usr/bin/arm-none-eabi-
-    endif
+_HOST_OSNAME!=	uname -s
 
-    LIBGCC=	`${CC} ${CMACHCPU} -print-libgcc-file-name`
+ARM_GCC_PREFIX!=if [ x"${_HOST_OSNAME}" = x"OpenBSD" ] ; then \
+			echo "/usr/local/bin/arm-none-eabi" ; \
+		elif [ x"${_HOST_OSNAME}" = x"Linux" ] ; then \
+			echo "/usr/bin/arm-none-eabi" ; \
+		else \
+			echo "/does/not/exist" ; \
+		fi
 
-endif
+_AS=		-gcc
+_CC=		-gcc
+_CPP=		-cpp
+_LD=		-ld
+_SIZE=		-size
+_OBJCOPY=	-objcopy
+_OBJDUMP=	-objdump
 
 AS=		${ARM_GCC_PREFIX}${_AS}
 CC=		${ARM_GCC_PREFIX}${_CC}
@@ -37,3 +26,5 @@ LD=		${ARM_GCC_PREFIX}${_LD}
 SIZE=		${ARM_GCC_PREFIX}${_SIZE}
 OBJCOPY=	${ARM_GCC_PREFIX}${_OBJCOPY}
 OBJDUMP=	${ARM_GCC_PREFIX}${_OBJDUMP}
+
+LIBGCC=		`${CC} ${CMACHCPU} -print-libgcc-file-name`

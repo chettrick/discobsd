46ff37cac50f72fcf2ff3f335446c389bbf7f853
Chris Hettrick <git@structfoo.com>
2023-04-22 21:39:41 -0700

    sys/pic32: Portable GCC sys/pic32/gcc-config.mk makefile.
    Remove all old and untested toolchains and keep only GCC.
    Toolchains can be re-added once they are vetted.
    
    A few tricks to remove the gmake-isms from gcc-config.mk and make
    it portable with gmake and the various BSD makes.
    
    Determine the host operating system name to select the path to the
    GCC mips-elf compiler tools, and fail with /does/not/exist-.
    
    != runs the shell commands and saves the output to MIPS_GCC_PREFIX.
    
    On the command line a different tool prefix can be used like:
      $ make MIPS_GCC_PREFIX="/path/to/my/mips-elf-"

--- sys/pic32/gcc-config.mk
+++ sys/pic32/gcc-config.mk
@@ -1,89 +1,19 @@
-# chipKIT PIC32 compiler from UECIDE
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Use UECIDE package from http://uecide.org/download
-ifndef MIPS_GCC_PREFIX
-    ifdef UECIDE
-		ifeq ($(UECIDE)/compilers/pic32-tools-452/bin/pic32-gcc,$(wildcard $(UECIDE)/compilers/pic32-tools-452/bin/pic32-gcc))
-			MIPS_GCC_PREFIX = $(UECIDE)/compilers/pic32-tools-452/bin/pic32-
-			MIPS_GCC_FORMAT = elf32-tradlittlemips
-		endif
-    endif
-endif
+# GCC mips-elf toolchain on OpenBSD, FreeBSD, and Linux
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Build the toolchain with the Ports Collection for OpenBSD,
+# as described in the file: tools/openbsd/README.md
+#
+# Build the toolchain from sources for FreeBSD and Linux,
+# as described on page: http://retrobsd.org/wiki/doku.php/doc/toolchain-mips
 
-# chipKIT PIC32 compiler on Linux
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Download from https://github.com/jasonkajita/chipKIT-cxx/downloads
-# and unzip to /usr/local.
-# Need to copy pic32-tools/pic32mx/include/stdarg.h
-# to pic32-tools/lib/gcc/pic32mx/4.5.1/include.
-# MPLABX C32 compiler doesn't support some functionality
-# we need, so use chipKIT compiler by default.
-ifndef MIPS_GCC_PREFIX
-    ifeq (/usr/local/pic32-tools/bin/pic32-gcc,$(wildcard /usr/local/pic32-tools/bin/pic32-gcc))
-        MIPS_GCC_PREFIX = /usr/local/pic32-tools/bin/pic32-
-        MIPS_GCC_FORMAT = elf32-tradlittlemips
-    endif
-endif
+_HOST_OSNAME!=	uname -s
 
-# Generic MIPS toolchain
-# ~~~~~~~~~~~~~~~~~~~~~~
-# You can build it from sources, as described on page
-# http://retrobsd.org/wiki/doku.php/doc/toolchain-mips
-ifndef MIPS_GCC_PREFIX
-    ifeq (/usr/local/mips-gcc-4.8.1/bin/mips-elf-gcc,$(wildcard /usr/local/mips-gcc-4.8.1/bin/mips-elf-gcc))
-        MIPS_GCC_PREFIX = /usr/local/mips-gcc-4.8.1/bin/mips-elf-
-    endif
-endif
-
-# Generic MIPS toolchain on FreeBSD
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# You can build it from sources, as described on page
-# http://retrobsd.org/wiki/doku.php/doc/toolchain-mips
-# Maybe you can install it from packages one day too.
-ifndef MIPS_GCC_PREFIX
-    ifeq (/usr/local/mips-elf/bin/mips-elf-gcc,$(wildcard /usr/local/mips-elf/bin/mips-elf-gcc))
-        MIPS_GCC_PREFIX = /usr/local/mips-elf/bin/mips-elf-
-    endif
-endif
-
-# Generic MIPS toolchain on OpenBSD
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ifndef MIPS_GCC_PREFIX
-    ifeq (/usr/local/bin/mips-elf-gcc,$(wildcard /usr/local/bin/mips-elf-gcc))
-        MIPS_GCC_PREFIX = /usr/local/bin/mips-elf-
-    endif
-endif
-
-# Mentor Sourcery CodeBench Lite toolchain
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ifndef MIPS_GCC_PREFIX
-    # Download a Linux binary package from
-    # https://sourcery.mentor.com/GNUToolchain/release2641
-    ifeq (/usr/local/mips-2013.11/bin/mips-sde-elf-gcc,$(wildcard /usr/local/mips-2013.11/bin/mips-sde-elf-gcc))
-        MIPS_GCC_PREFIX = /usr/local/mips-2013.11/bin/mips-sde-elf-
-        MIPS_GCC_FORMAT = elf32-tradlittlemips
-    endif
-endif
-ifndef MIPS_GCC_PREFIX
-    # Download a Linux binary package from
-    # https://sourcery.mentor.com/GNUToolchain/release2774
-    ifeq (/usr/local/mips-2014.05/bin/mips-sde-elf-gcc,$(wildcard /usr/local/mips-2014.05/bin/mips-sde-elf-gcc))
-        MIPS_GCC_PREFIX = /usr/local/mips-2014.05/bin/mips-sde-elf-
-        MIPS_GCC_FORMAT = elf32-tradlittlemips
-    endif
-endif
-
-# Imagination Codescape MIPS SDK Essentials
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Download a Linux binary package from:
-# http://community.imgtec.com/developers/mips/tools/codescape-mips-sdk/download-codescape-mips-sdk-essentials/
-ifndef MIPS_GCC_ROOT
-    ifeq (/opt/imgtec/Toolchains/mips-mti-elf/2015.01-7,$(wildcard /opt/imgtec/Toolchains/mips-mti-elf/2015.01-7))
-        MIPS_GCC_ROOT   = /opt/imgtec/Toolchains/mips-mti-elf/2015.01-7
-        MIPS_GCC_FORMAT = elf32-tradlittlemips
-    endif
-endif
-
-ifndef MIPS_GCC_PREFIX
-    $(error Unable to locate any GCC MIPS toolchain!)
-endif
+MIPS_GCC_PREFIX!= if [ x"${_HOST_OSNAME}" = x"OpenBSD" ] ; then \
+			echo "/usr/local/bin/mips-elf-" ; \
+		elif [ x"${_HOST_OSNAME}" = x"FreeBSD" ] ; then \
+			echo "/usr/local/mips-elf/bin/mips-elf-" ; \
+		elif [ x"${_HOST_OSNAME}" = x"Linux" ] ; then \
+			echo "/usr/local/mips-gcc-4.8.1/bin/mips-elf-" ; \
+		else \
+			echo "/does/not/exist-" ; \
+		fi

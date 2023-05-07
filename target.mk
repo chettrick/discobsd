1bf74f8b3eba4b6daadb2cd428f1185bd6ca7f7f
Chris Hettrick <git@structfoo.com>
2023-05-06 22:19:39 -0700

    build: Portable GCC-only target.mk makefile.
    Remove all old and untested MIPS toolchains and keep only GCC.
    Toolchains can be re-added once they are vetted.
    
    A few tricks to remove gmake-isms from target.mk and make
    it portable with GNU make and the various BSD makes.
    
    Replace the $(shell ...) gmake-ism with variables derived from
    running the shell with the "!=" variable assignment operator.
    
    The structure and commands have largely stayed the same as before,
    as this commit is mainly mechanical in nature.
    
    The "-" at the end of ${GCCPREFIX} is no longer; it is now the
    suffix's responsibility. Adjust all uses of ${GCCPREFIX} accordingly.

--- target.mk
+++ target.mk
@@ -4,175 +4,82 @@
 MACHINE=	stm32
 MACHINE_ARCH=	arm
 
-DESTDIR		?= ${TOPSRC}/distrib/obj/destdir.${MACHINE}
-RELEASE		= 0.0
-BUILD		= $(shell git rev-list HEAD --count)
-VERSION		= $(RELEASE)-$(BUILD)
+DESTDIR?=	${TOPSRC}/distrib/obj/destdir.${MACHINE}
+RELEASE=	0.0
+BUILD!=		git rev-list HEAD --count
+VERSION=	${RELEASE}-${BUILD}
 
-UNAME_S		= $(shell uname -s)
-#USE_GROFF	= 1
+_HOST_OSNAME!=	uname -s
 
-ifeq ($(MACHINE_ARCH), mips)
+GCCPREFIX!=if [ x"${MACHINE_ARCH}" = x"arm" ] ; then \
+		if [ x"${_HOST_OSNAME}" = x"OpenBSD" ] ; then \
+			echo "/usr/local/bin/arm-none-eabi" ; \
+		elif [ x"${_HOST_OSNAME}" = x"Linux" ] ; then \
+			echo "/usr/bin/arm-none-eabi" ; \
+		else \
+			echo "/does/not/exist" ; \
+		fi \
+	elif [ x"${MACHINE_ARCH}" = x"mips" ] ; then \
+		if [ x"${_HOST_OSNAME}" = x"OpenBSD" ] ; then \
+			echo "/usr/local/bin/mips-elf" ; \
+		elif [ x"${_HOST_OSNAME}" = x"FreeBSD" ] ; then \
+			echo "/usr/local/mips-elf/bin/mips-elf" ; \
+		elif [ x"${_HOST_OSNAME}" = x"Linux" ] ; then \
+			echo "/usr/local/mips-gcc-4.8.1/bin/mips-elf" ; \
+		else \
+			echo "/does/not/exist" ; \
+		fi \
+	else \
+		echo "/does/not/exist" ; \
+	fi
 
-# chipKIT PIC32 compiler from UECIDE
-ifdef UECIDE
-ifndef GCCPREFIX
-	ifeq ($(UECIDE)/compilers/pic32-tools-452/bin/pic32-gcc,$(wildcard $(UECIDE)/compilers/pic32-tools-452/bin/pic32-gcc))
-		GCCPREFIX   = ${UECIDE}/compilers/pic32-tools-452/bin/pic32-
-		LDFLAGS     = -Wl,--oformat=elf32-tradlittlemips
-		INCLUDES    = -I${UECIDE}/compilers/pic32-tools-452/lib/gcc/pic32mx/4.5.2/include
-	endif
-endif
-endif
+CC!=	if [ x"${MACHINE_ARCH}" = x"arm" ] ; then \
+		echo "${GCCPREFIX}-gcc -mcpu=cortex-m4 -mabi=aapcs -mlittle-endian -mthumb -mfloat-abi=soft -nostdinc -I${TOPSRC}/include ${INCLUDES}" ; \
+	elif [ x"${MACHINE_ARCH}" = x"mips" ] ; then \
+		echo "${GCCPREFIX}-gcc -mips32r2 -EL -msoft-float -nostdinc -fshort-double -I${TOPSRC}/include ${INCLUDES}" ; \
+	else \
+		echo "/does/not/exist" ; \
+	fi
 
-# chipKIT PIC32 compiler on Linux
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Download from https://github.com/jasonkajita/chipKIT-cxx/downloads
-# and unzip to /usr/local.
-# Need to copy pic32-tools/pic32mx/include/stdarg.h
-# to pic32-tools/lib/gcc/pic32mx/4.5.1/include.
-# MPLABX C32 compiler doesn't support some functionality
-# we need, so use chipKIT compiler by default.
-ifndef GCCPREFIX
-ifeq (/usr/local/pic32-tools/bin/pic32-gcc,$(wildcard /usr/local/pic32-tools/bin/pic32-gcc))
-    GCCPREFIX   = /usr/local/pic32-tools/bin/pic32-
-    LDFLAGS     = -Wl,--oformat=elf32-tradlittlemips
-    INCLUDES    = -I/usr/local/pic32-tools/lib/gcc/pic32mx/4.5.1/include
-endif
-endif
-
-# Generic MIPS toolchain
-# ~~~~~~~~~~~~~~~~~~~~~~
-# You can build it from sources, as described on page
-# http://retrobsd.org/wiki/doku.php/doc/toolchain-mips
-ifndef GCCPREFIX
-ifeq (/usr/local/mips-gcc-4.8.1/bin/mips-elf-gcc,$(wildcard /usr/local/mips-gcc-4.8.1/bin/mips-elf-gcc))
-    GCCPREFIX   = /usr/local/mips-gcc-4.8.1/bin/mips-elf-
-    LDFLAGS     =
-    INCLUDES    =
-endif
-endif
-
-# Generic MIPS toolchain on FreeBSD
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# You can build it from sources, as described on page
-# http://retrobsd.org/wiki/doku.php/doc/toolchain-mips
-# Maybe you can install it from packages one day too.
-ifndef GCCPREFIX
-ifeq (/usr/local/mips-elf/bin/mips-elf-gcc,$(wildcard /usr/local/mips-elf/bin/mips-elf-gcc))
-    GCCPREFIX   = /usr/local/mips-elf/bin/mips-elf-
-    LDFLAGS     =
-    INCLUDES    =
-endif
-endif
-
-# Generic MIPS toolchain on OpenBSD
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ifndef GCCPREFIX
-ifeq (/usr/local/bin/mips-elf-gcc,$(wildcard /usr/local/bin/mips-elf-gcc))
-    GCCPREFIX   = /usr/local/bin/mips-elf-
-    LDFLAGS     =
-    INCLUDES    =
-endif
-endif
-
-# Mentor Sourcery CodeBench Lite toolchain
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# You can download a Linux or Windows binary package from
-# https://sourcery.mentor.com/GNUToolchain/release2641
-ifndef GCCPREFIX
-ifeq (/usr/local/mips-2013.11/bin/mips-sde-elf-gcc,$(wildcard /usr/local/mips-2013.11/bin/mips-sde-elf-gcc))
-    GCCPREFIX   = /usr/local/mips-2013.11/bin/mips-sde-elf-
-    LDFLAGS     = -Wl,--oformat=elf32-tradlittlemips
-    INCLUDES    =
-endif
-endif
-ifndef GCCPREFIX
-ifeq (/usr/local/mips-2014.05/bin/mips-sde-elf-gcc,$(wildcard /usr/local/mips-2014.05/bin/mips-sde-elf-gcc))
-    GCCPREFIX   = /usr/local/mips-2014.05/bin/mips-sde-elf-
-    LDFLAGS     = -Wl,--oformat=elf32-tradlittlemips
-    INCLUDES    =
-endif
-endif
-
-endif # MACHINE_ARCH == mips
-ifeq ($(MACHINE_ARCH), arm)
-
-# Generic ARM toolchain on OpenBSD
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# You can install the arm-none-eabi package, Linaro version, via:
-#   $ doas pkg_add arm-none-eabi-gcc-linaro
-# This also installs binutils and newlib for arm-none-eabi target.
-# You can install gdb for arm-none-eabi via:
-#   $ doas pkg_add arm-none-eabi-gdb
-# Note that the arm-none-eabi version of gdb currently (as of OpenBSD 6.6)
-# conflicts with the standard version of gdb from the ports tree,
-# so choose only one to install on your system at a time.
-ifndef GCCPREFIX
-ifeq (/usr/local/bin/arm-none-eabi-gcc,$(wildcard /usr/local/bin/arm-none-eabi-gcc))
-    GCCPREFIX   = /usr/local/bin/arm-none-eabi-
-    LDFLAGS     =
-    INCLUDES    =
-endif
-# Generic ARM toolchain on Linux
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ifeq (/usr/bin/arm-none-eabi-gcc,$(wildcard /usr/bin/arm-none-eabi-gcc))
-    GCCPREFIX   = /usr/bin/arm-none-eabi-
-    LDFLAGS     =
-    INCLUDES    =
-endif
-endif
-
-endif # MACHINE_ARCH == arm
-
-ifndef GCCPREFIX
-   $(error Unable to locate any GCC $(MACHINE_ARCH) toolchain!)
-endif
-
-CFLAGS		= -Os
-
-LIBS		= -lc
-
-ifeq ($(MACHINE_ARCH), mips)
-
-CC		= $(GCCPREFIX)gcc -mips32r2 -EL -msoft-float -nostdinc -fshort-double -I$(TOPSRC)/include $(INCLUDES)
-OBJDUMP         = $(GCCPREFIX)objdump -mmips:isa32r2
-LDFLAGS		+= -N -nostartfiles -fno-dwarf2-cfi-asm -T$(TOPSRC)/lib/elf32-mips.ld \
-		   $(TOPSRC)/lib/crt0.o -L$(TOPSRC)/lib
 # Enable mips16e instruction set by default
-CFLAGS		+= -mips16
+CFLAGS!=if [ x"${MACHINE_ARCH}" = x"arm" ] ; then \
+		echo "-Os" ; \
+	elif [ x"${MACHINE_ARCH}" = x"mips" ] ; then \
+		echo "-Os -mips16" ; \
+	else \
+		echo "" ; \
+	fi
 
-endif # MACHINE_ARCH == mips
-ifeq ($(MACHINE_ARCH), arm)
+LDFLAGS=-N -nostartfiles -fno-dwarf2-cfi-asm -T${TOPSRC}/lib/elf32-${MACHINE_ARCH}.ld ${TOPSRC}/lib/crt0.o -L${TOPSRC}/lib
 
-CC		= $(GCCPREFIX)gcc -mcpu=cortex-m4 -mabi=aapcs -mlittle-endian -mthumb -mfloat-abi=soft -nostdinc -I$(TOPSRC)/include $(INCLUDES)
-OBJDUMP         = $(GCCPREFIX)objdump -marm -M force-thumb
-LDFLAGS		+= -N -nostartfiles -fno-dwarf2-cfi-asm -T$(TOPSRC)/lib/elf32-arm.ld \
-		   $(TOPSRC)/lib/crt0.o -L$(TOPSRC)/lib
+LIBS=	-lc
 
-endif # MACHINE_ARCH == arm
+OBJDUMP!=if [ x"${MACHINE_ARCH}" = x"arm" ] ; then \
+		echo "${GCCPREFIX}-objdump -marm -M force-thumb" ; \
+	elif [ x"${MACHINE_ARCH}" = x"mips" ] ; then \
+		echo "${GCCPREFIX}-objdump -mmips:isa32r2" ; \
+	else \
+		echo "/does/not/exist" ; \
+	fi
 
-LD		= $(GCCPREFIX)ld
-AR		= $(GCCPREFIX)ar
-RANLIB          = $(GCCPREFIX)ranlib
-SIZE            = $(GCCPREFIX)size
-AS		= $(CC) -x assembler-with-cpp -c
+YACC!=	if [ x"${_HOST_OSNAME}" = x"Linux" ] ; then \
+		echo "byacc" ; \
+	else \
+		echo "yacc" ; \
+	fi
 
-ifeq ($(UNAME_S), Linux)
-YACC            = byacc
-else
-YACC            = yacc
-endif
+LD=		${GCCPREFIX}-ld
+AR=		${GCCPREFIX}-ar
+RANLIB=		${GCCPREFIX}-ranlib
+SIZE=		${GCCPREFIX}-size
+AS=		${CC} -x assembler-with-cpp -c
 
-LEX             = flex
-INSTALL		= install -m 644
-INSTALLDIR	= install -m 755 -d
-TAGSFILE	= tags
+LEX=		flex
+INSTALL=	install -m 644
+INSTALLDIR=	install -m 755 -d
+TAGSFILE=	tags
 
-ifdef USE_GROFF
-MANROFF		= nroff -man -h -Tascii
-else
-MANROFF		= mandoc -Tascii
-endif
+#MANROFF=	nroff -man -h -Tascii
+MANROFF=	mandoc -Tascii
 
-ELF2AOUT	= $(TOPSRC)/tools/elf2aout/elf2aout
+ELF2AOUT=	${TOPSRC}/tools/elf2aout/elf2aout

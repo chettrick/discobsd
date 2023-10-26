e5741a6035377f9957f6503ded4361141fc0a667
Chris Hettrick <git@structfoo.com>
2023-10-26 13:13:33 -0600

    build: Enable -fcommon for base and kernel builds for mips.
    gcc 10+ defaults to -fno-common, which produces many "multiple
    definition of ..." errors throughout the tree.
    
    Enable -fcommon for mips/pic32 until tree is -fno-common clean.
    
    Modern OpenBSD, FreeBSD, and Ubuntu are now able to build the
    base and kernel for pic32 with a mips-elf-gcc 10+ compiler.
    
    Remove the dubious -fshort-double CFLAG, as it was removed from
    mips-elf-gcc a while ago and modern compilers do not support it.

--- target.mk
+++ target.mk
@@ -43,7 +43,7 @@ GCCPREFIX!=if [ x"${MACHINE_ARCH}" = x"arm" ] ; then \
 CC!=	if [ x"${MACHINE_ARCH}" = x"arm" ] ; then \
 		echo "${GCCPREFIX}-gcc -mcpu=cortex-m4 -mabi=aapcs -mlittle-endian -mthumb -mfloat-abi=soft -nostdinc -I${TOPSRC}/include ${INCLUDES}" ; \
 	elif [ x"${MACHINE_ARCH}" = x"mips" ] ; then \
-		echo "${GCCPREFIX}-gcc -mips32r2 -EL -msoft-float -nostdinc -fshort-double -I${TOPSRC}/include ${INCLUDES}" ; \
+		echo "${GCCPREFIX}-gcc -mips32r2 -EL -msoft-float -nostdinc -I${TOPSRC}/include ${INCLUDES}" ; \
 	else \
 		echo "/does/not/exist" ; \
 	fi
@@ -52,7 +52,7 @@ CC!=	if [ x"${MACHINE_ARCH}" = x"arm" ] ; then \
 COPTS!=if [ x"${MACHINE_ARCH}" = x"arm" ] ; then \
 		echo "-Os -fcommon" ; \
 	elif [ x"${MACHINE_ARCH}" = x"mips" ] ; then \
-		echo "-Os -mips16" ; \
+		echo "-Os -fcommon -mips16" ; \
 	else \
 		echo "" ; \
 	fi

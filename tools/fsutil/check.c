27d12097afec1b3a286d9a0617ad90ac2cbe3398
Chris Hettrick <git@structfoo.com>
2024-11-12 22:08:56 -0700

    tools/fsutil: Prototype function definition.
    Modern Clang warns for function definitions without prototype.
    
    -Wdeprecated-non-prototype
    
    Cleans up this warning.

--- tools/fsutil/check.c
+++ tools/fsutil/check.c
@@ -335,7 +335,7 @@ static int scan_directory (fs_inode_t *inode, unsigned blk, void *arg)
 {
     fs_dirent_t direntry;
     unsigned char *dirp;
-    int (*func) () = arg;
+    int (*func) (fs_t *, fs_dirent_t *) = arg;
     int n;
 
 /*printf ("scan_directory: I=%d, blk=%d\n", inode->number, blk);*/

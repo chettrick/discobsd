b8f8ef65287e149bcf151592178a3b48b4e99d10
Chris Hettrick <git@structfoo.com>
2025-01-11 10:12:20 -0700

    libc: 64-bit double and long double on stm32.
    On stm32, floats are 32-bits, doubles and long doubles are 64-bits.
    
    LDBL_* defines are the same as DBL_* defines.
    
    Update sys/stm32/_float.h to the correct floating-point defines.

--- sys/stm32/_float.h
+++ sys/stm32/_float.h
@@ -20,31 +20,29 @@
 #define FLT_MIN_EXP     (-125)          /* emin */
 
 /*
- * These values provide information pertaining to the double type
- * The values are dependant upon the presence of the -fno-short-double
- * compiler option.
+ * These values provide information pertaining to the double type.
  */
-#define DBL_EPSILON     FLT_EPSILON
-#define DBL_MAX         FLT_MAX
-#define DBL_MIN         FLT_MIN
-#define DBL_DIG         FLT_DIG
-#define DBL_MANT_DIG    FLT_MANT_DIG
-#define DBL_MAX_10_EXP  FLT_MAX_10_EXP
-#define DBL_MAX_EXP     FLT_MAX_EXP
-#define DBL_MIN_10_EXP  FLT_MIN_10_EXP
-#define DBL_MIN_EXP     FLT_MIN_EXP
+#define DBL_EPSILON     2.2204460492503131E-16
+#define DBL_MAX         1.7976931348623157E+308
+#define DBL_MIN         2.2250738585072014E-308
+#define DBL_DIG         15
+#define DBL_MANT_DIG    53
+#define DBL_MAX_10_EXP  308
+#define DBL_MAX_EXP     1024
+#define DBL_MIN_10_EXP  (-307)
+#define DBL_MIN_EXP     (-1021)
 
 /*
  * These values provide information pertaining to the long double type.
  */
-#define LDBL_EPSILON    2.2204460492503131E-16
-#define LDBL_MAX        1.7976931348623157E+308
-#define LDBL_MIN        2.2250738585072014E-308
-#define LDBL_DIG        15
-#define LDBL_MANT_DIG   53
-#define LDBL_MAX_10_EXP 308
-#define LDBL_MAX_EXP    1024
-#define LDBL_MIN_10_EXP (-307)
-#define LDBL_MIN_EXP    (-1021)
+#define LDBL_EPSILON    DBL_EPSILON
+#define LDBL_MAX        DBL_MAX
+#define LDBL_MIN        DBL_MIN
+#define LDBL_DIG        DBL_DIG
+#define LDBL_MANT_DIG   DBL_MANT_DIG
+#define LDBL_MAX_10_EXP DBL_MAX_10_EXP
+#define LDBL_MAX_EXP    DBL_MAX_EXP
+#define LDBL_MIN_10_EXP DBL_MIN_10_EXP
+#define LDBL_MIN_EXP    DBL_MIN_EXP
 
 #endif /* _MACHINE__FLOAT_H_ */

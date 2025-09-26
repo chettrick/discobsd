2e199a9c5b2913ad58e237b22117cf20ccc6b82a
Chris Hettrick <git@structfoo.com>
2025-09-26 01:18:49 -0600

    knf: stm32 conf.c; no binary change.
    ANSI and de-register, with style(9) whitespace and format changes.
    No C++ style comments. Clean up other comments; full sentences.
    
    Checked with knfmt(1).
    
    nulldev() has an empty parameter list, as it is a function pointer
    used in place for functions that have varied parameter lists.
    It is currently being used for these character device functions:
    stop(struct tty *, int);
    open(dev_t, int, int);
    close(dev_t, int, int);
    Not having a defined parameter list has been allowed in the C spec,
    but may be removed in the near future.
    Note that nulldev() and nulldev(void) are _not_ the same.
    
    Added back the 2.11BSD SCCS version tag.

--- sys/arch/stm32/stm32/conf.c
+++ sys/arch/stm32/stm32/conf.c
@@ -2,6 +2,8 @@
  * Copyright (c) 1986 Regents of the University of California.
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
+ *
+ *	@(#)conf.c	3.2 (2.11BSD GTE) 1997/11/12
  */
 #include <sys/param.h>
 #include <sys/conf.h>
@@ -24,60 +26,57 @@
 extern int strcmp(char *s1, char *s2);
 
 #ifdef SD_ENABLED
-#   include <stm32/dev/sd.h>
+#include <stm32/dev/sd.h>
 #endif
+
 #ifdef PTY_ENABLED
-#   include <sys/pty.h>
+#include <sys/pty.h>
 #endif
+
 #ifdef SDIO_ENABLED
-#   include <stm32/dev/sdio.h>
+#include <stm32/dev/sdio.h>
 #endif
 
 /*
  * Null routine; placed in insignificant entries
  * in the bdevsw and cdevsw tables.
  */
-int nulldev()
+int
+nulldev()	/* XXX */
 {
-    return 0;
+	return 0;
 }
 
-int noopen(dev, flag, mode)
-    dev_t dev;
-    int flag, mode;
+int
+noopen(dev_t dev, int flag, int mode)
 {
-    return ENXIO;
+	return ENXIO;
 }
 
-int norw(dev, uio, flag)
-    dev_t dev;
-    struct uio *uio;
-    int flag;
+int
+norw(dev_t dev, struct uio *uio, int flag)
 {
-    return 0;
+	return 0;
 }
 
-int noioctl(dev, cmd, data, flag)
-    dev_t dev;
-    u_int cmd;
-    caddr_t data;
-    int flag;
+int
+noioctl(dev_t dev, u_int cmd, caddr_t data, int flag)
 {
-    return EIO;
+	return EIO;
 }
 
 /*
- * root attach routine
+ * root attach routine.
  */
-daddr_t nosize(dev)
-    dev_t dev;
+daddr_t
+nosize(dev_t dev)
 {
-    return 0;
+	return 0;
 }
 
 #define NOBDEV \
-    noopen,         noopen,         nostrategy, \
-    nosize,         noioctl,
+	noopen,		noopen,		nostrategy, \
+	nosize,		noioctl,
 
 /*
  * The RetroDisks require the same master number as the disk entry in the
@@ -85,164 +84,170 @@ daddr_t nosize(dev)
  * devices must be numbered from master 0 upwards.
  */
 const struct bdevsw bdevsw[] = {
-{   /* 0 - sd */
+	{	/* 0 - sd */
 #ifdef SD_ENABLED
-    sdopen,         sdclose,        sdstrategy,
-    sdsize,         sdioctl,
+		sdopen,		sdclose,	sdstrategy,
+		sdsize,		sdioctl,
 #else
-    NOBDEV
+		NOBDEV
 #endif
-},
-{   /* 1 - sramc */
-    NOBDEV
-},
-{   /* 2 - sdramp */
-    NOBDEV
-},
-{   /* 3 - mrams */
-    NOBDEV
-},
-{   /* 4 - swap */
-    swopen,         swclose,        swstrategy,
-    swsize,         swcioctl,
-},
-{   /* 5 - spirams */
-    NOBDEV
-},
+	},
+	{	/* 1 - sramc */
+		NOBDEV
+	},
+	{	/* 2 - sdramp */
+		NOBDEV
+	},
+	{	/* 3 - mrams */
+		NOBDEV
+	},
+	{	/* 4 - swap */
+		swopen,		swclose,	swstrategy,
+		swsize,		swcioctl,
+	},
+	{	/* 5 - spirams */
+		NOBDEV
+	},
 
-{ 0 },
+	/*
+	 * End the list with a blank entry.
+	 */
+	{ 0 },
 };
 
 const int nblkdev = sizeof(bdevsw) / sizeof(bdevsw[0]) - 1;
 
 #define NOCDEV \
-    noopen,         noopen,         norw,           norw, \
-    noioctl,        nulldev,        0,              seltrue, \
-    nostrategy,     0,              0,
+	noopen,		noopen,		norw,		norw, \
+	noioctl,	nulldev,	0,		seltrue, \
+	nostrategy,	0,		0,
 
 const struct cdevsw cdevsw[] = {
-
-/*
- * Static drivers - every system has these:
- */
-{   /* 0 - console */
-    cnopen,         cnclose,        cnread,         cnwrite,
-    cnioctl,        nulldev,        cnttys,         cnselect,
-    nostrategy,     0,              0,
-},
-{   /* 1 - mem, kmem, null, zero */
+	/*
+	 * Static drivers - every system has these:
+	 */
+	{	/* 0 - console */
+		cnopen,		cnclose,	cnread,		cnwrite,
+		cnioctl,	nulldev,	cnttys,		cnselect,
+		nostrategy,	0,		0,
+	},
+	{	/* 1 - mem, kmem, null, zero */
 #if MEM_MAJOR != 1
-#   error Wrong MEM_MAJOR value!
+#error Wrong MEM_MAJOR value!
 #endif
-    nulldev,        nulldev,        mmrw,           mmrw,
-    noioctl,        nulldev,        0,              seltrue,
-    nostrategy,     0,              0,
-},
-{   /* 2 - tty */
-    syopen,         nulldev,        syread,         sywrite,
-    syioctl,        nulldev,        0,              syselect,
-    nostrategy,     0,              0,
-},
-{   /* 3 - fd */
-    fdopen,         nulldev,        norw,           norw,
-    noioctl,        nulldev,        0,              seltrue,
-    nostrategy,     0,              0,
-},
-{   /* 4 - temp (temporary allocation in swap space) */
-    swcopen,        swcclose,       swcread,        swcwrite,
-    swcioctl,       nulldev,        0,              seltrue,
-    nostrategy,     0,              0,
-},
+		nulldev,	nulldev,	mmrw,		mmrw,
+		noioctl,	nulldev,	0,		seltrue,
+		nostrategy,	0,		0,
+	},
+	{	/* 2 - tty */
+		syopen,		nulldev,	syread,		sywrite,
+		syioctl,	nulldev,	0,		syselect,
+		nostrategy,	0,		0,
+	},
+	{	/* 3 - fd */
+		fdopen,		nulldev,	norw,		norw,
+		noioctl,	nulldev,	0,		seltrue,
+		nostrategy,	0,		0,
+	},
+	{	/* 4 - temp (temporary allocation in swap space) */
+		swcopen,	swcclose,	swcread,	swcwrite,
+		swcioctl,	nulldev,	0,		seltrue,
+		nostrategy,	0,		0,
+	},
 
-/*
- * Optional drivers from here on:
- */
-{   /* 5 - log */
+	/*
+	 * Optional drivers from here on:
+	 */
+	{	/* 5 - log */
 #ifdef LOG_ENABLED
-    logopen,        logclose,       logread,        norw,
-    logioctl,       nulldev,        0,              logselect,
-    nostrategy,     0,              0,
+		logopen,	logclose,	logread,	norw,
+		logioctl,	nulldev,	0,		logselect,
+		nostrategy,	0,		0,
 #else
-    NOCDEV
+		NOCDEV
 #endif
-},
-{   /* 6 - tty uart */
+	},
+	{	/* 6 - tty uart */
 #if UART_MAJOR != 6
-#   error Wrong UART_MAJOR value!
+#error Wrong UART_MAJOR value!
 #endif
 #if defined(UART1_ENABLED) || defined(UART2_ENABLED) || \
     defined(UART3_ENABLED) || defined(UART4_ENABLED) || \
     defined(UART5_ENABLED) || defined(UART6_ENABLED)
-    uartopen,       uartclose,      uartread,       uartwrite,
-    uartioctl,      nulldev,        uartttys,       uartselect,
-    nostrategy,     uartgetc,       uartputc,
+		uartopen,	uartclose,	uartread,	uartwrite,
+		uartioctl,	nulldev,	uartttys,	uartselect,
+		nostrategy,	uartgetc,	uartputc,
 #else
-    NOCDEV
+		NOCDEV
 #endif
-},
-{   /* 7 - tty usb */
-    NOCDEV
-},
-{   /* 8, 9 - pty */
+	},
+	{	/* 7 - tty usb */
+		NOCDEV
+	},
+	{	/* 8, 9 - pty */
 #ifdef PTY_ENABLED
-    ptsopen,        ptsclose,       ptsread,        ptswrite,
-    ptyioctl,       nulldev,        pt_tty,         ptcselect,
-    nostrategy,     0,              0,
-}, {
-    ptcopen,        ptcclose,       ptcread,        ptcwrite,
-    ptyioctl,       nulldev,        pt_tty,         ptcselect,
-    nostrategy,     0,              0,
+		ptsopen,	ptsclose,	ptsread,	ptswrite,
+		ptyioctl,	nulldev,	pt_tty,		ptcselect,
+		nostrategy,	0,		0,
+	},
+	{
+		ptcopen,	ptcclose,	ptcread,	ptcwrite,
+		ptyioctl,	nulldev,	pt_tty,		ptcselect,
+		nostrategy,	0,		0,
 #else
-    NOCDEV }, { NOCDEV
+		NOCDEV
+	},
+	{
+		NOCDEV
 #endif
-},
-{   /* 10 - gpio */
-    NOCDEV
-},
-{   /* 11 - adc */
-    NOCDEV
-},
-{   /* 12 - spi */
+	},
+	{	/* 10 - gpio */
+		NOCDEV
+	},
+	{	/* 11 - adc */
+		NOCDEV
+	},
+	{	/* 12 - spi */
 #if defined(SPI1_ENABLED) || defined(SPI2_ENABLED) || \
     defined(SPI3_ENABLED) || defined(SPI4_ENABLED) || \
     defined(SPI5_ENABLED) || defined(SPI6_ENABLED) || \
     defined(SPI7_ENABLED) || defined(SPI8_ENABLED)
-    spi_open,       spi_close,      spi_read,       spi_write,
-    spi_ioctl,      nulldev,        0,              seltrue,
-    nostrategy,     0,              0,
+		spi_open,	spi_close,	spi_read,	spi_write,
+		spi_ioctl,	nulldev,	0,		seltrue,
+		nostrategy,	0,		0,
 #else
-    NOCDEV
+		NOCDEV
 #endif
-},
-{   /* 13 - glcd */
-    NOCDEV
-},
-{   /* 14 - pwm */
-    NOCDEV
-},
-{   /* 15 - picga */            // Ignore this for now - it's WIP.
-    NOCDEV
-},
-{   /* 16 - hxtft */
-    NOCDEV
-},
-{   /* 17 - skel */
-    NOCDEV
-},
-{   /* 18 - sdio */
+	},
+	{	/* 13 - glcd */
+		NOCDEV
+	},
+	{	/* 14 - pwm */
+		NOCDEV
+	},
+	{	/* 15 - picga */	/* Ignore this for now - it's WIP. */
+		NOCDEV
+	},
+	{	/* 16 - hxtft */
+		NOCDEV
+	},
+	{	/* 17 - skel */
+		NOCDEV
+	},
+	{	/* 18 - sdio */
 #ifdef SDIO_ENABLED
-    sdio_open,      sdio_close,     sdio_read,      sdio_write,
-    sdio_ioctl,     nulldev,        0,              seltrue,
-    nostrategy,     0,              0,
+		sdio_open,	sdio_close,	sdio_read,	sdio_write,
+		sdio_ioctl,	nulldev,	0,		seltrue,
+		nostrategy,	0,		0,
 #else
-    NOCDEV
+		NOCDEV
 #endif
-},
+	},
 
-/*
- * End the list with a blank entry
- */
-{ 0 },
+	/*
+	 * End the list with a blank entry.
+	 */
+	{ 0 },
 };
 
 const int nchrdev = sizeof(cdevsw) / sizeof(cdevsw[0]) - 1;
@@ -253,12 +258,12 @@ const int nchrdev = sizeof(cdevsw) / sizeof(cdevsw[0]) - 1;
  * A minimal stub routine can always return 0.
  */
 int
-iskmemdev(dev)
-    register dev_t dev;
+iskmemdev(dev_t dev)
 {
-    if (major(dev) == 1 && (minor(dev) == 0 || minor(dev) == 1))
-        return 1;
-    return 0;
+	if (major(dev) == 1 && (minor(dev) == 0 || minor(dev) == 1))
+		return 1;
+
+	return 0;
 }
 
 /*
@@ -267,24 +272,22 @@ iskmemdev(dev)
  * A minimal stub routine can always return 0.
  */
 int
-isdisk(dev, type)
-    dev_t dev;
-    register int type;
+isdisk(dev_t dev, int type)
 {
-    if (type != IFBLK)
-        return 0;
+	if (type != IFBLK)
+		return 0;
 
-    switch (major(dev)) {
-    case 0:                 /* rd0 */
-    case 1:                 /* rd1 */
-    case 2:                 /* rd2 */
-    case 3:                 /* rd3 */
-    case 4:                 /* sw */
-        return 1;
-    default:
-        return 0;
-    }
-    /* NOTREACHED */
+	switch (major(dev)) {
+	case 0:				/* rd0 */
+	case 1:				/* rd1 */
+	case 2:				/* rd2 */
+	case 3:				/* rd3 */
+	case 4:				/* sw */
+		return 1;
+	default:
+		return 0;
+	}
+	/* NOTREACHED */
 }
 
 /*
@@ -294,5 +297,5 @@ isdisk(dev, type)
 int
 chrtoblk(dev_t dev)
 {
-    return NODEV;
+	return NODEV;
 }

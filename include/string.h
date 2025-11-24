fa308a3fb8746741b5ced7f2e87a84989d995e68
Chris Hettrick <git@structfoo.com>
2025-11-23 23:01:06 -0700

    knf: include/string.h; no binary change.
    Whitespace knf changes following style(9) guidelines.
    
    Explicitly clarify that size_t is an unsigned int, not just unsigned.
    
    Added back the 2.11BSD SCCS version tag.

--- include/string.h
+++ include/string.h
@@ -2,47 +2,50 @@
  * Copyright (c) 1985 Regents of the University of California.
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
+ *
+ *	@(#)string.h	5.1.3 (2.11BSD) 1996/3/20
  */
-#ifndef NULL
-#define NULL    0
+
+#ifndef	NULL
+#define	NULL	0
 #endif
 
-#ifndef _SIZE_T
-#define _SIZE_T
-typedef unsigned size_t;
+#ifndef	_SIZE_T
+#define	_SIZE_T
+typedef	unsigned int	size_t;
 #endif
 
-char    *strcat (char *, const char *);
-char    *strncat (char *, const char *, size_t);
-size_t  strlcat (char *, const char *, size_t);
-char    *strcpy (char *, const char *);
-char    *strncpy (char *, const char *, size_t);
-size_t  strlcpy (char *, const char *, size_t);
+char		*strcat(char *, const char *);
+char		*strncat(char *, const char *, size_t);
+size_t		 strlcat(char *, const char *, size_t);
+char		*strcpy(char *, const char *);
+char		*strncpy(char *, const char *, size_t);
+size_t		 strlcpy(char *, const char *, size_t);
 
-char    *strstr (const char *, const char *);
+char		*strstr(const char *, const char *);
 
-int     strcmp (const char *, const char *);
-int     strncmp (const char *, const char *, size_t);
-size_t  strlen (const char *);
+int		 strcmp(const char *, const char *);
+int		 strncmp(const char *, const char *, size_t);
+size_t		 strlen(const char *);
 
-int     memcmp (const void *, const void *, size_t);
+int		 memcmp(const void *, const void *, size_t);
 
-void    *memmove (void *, const void *, size_t);
-void    *memccpy (void *, const void *, int, size_t);
-void    *memchr (const void *, int, size_t);
-void    *memcpy (void *, const void *, size_t);
-void    *memset (void *, int, size_t);
-char    *strchr (const char *, int);
+void		*memmove(void *, const void *, size_t);
+void		*memccpy(void *, const void *, int, size_t);
+void		*memchr(const void *, int, size_t);
+void		*memcpy(void *, const void *, size_t);
+void		*memset(void *, int, size_t);
+char		*strchr(const char *, int);
 
-char    *strdup (const char *);
-char    *strpbrk (const char *, const char *);
-char    *strrchr (const char *, int);
-char    *strsep (char **, const char *);
-char    *strtok (char *, const char *);
-char    *strtok_r (char *, const char *, char **);
+char		*strdup(const char *);
+char		*strpbrk(const char *, const char *);
+char		*strrchr(const char *, int);
+char		*strsep(char **, const char *);
+char		*strtok(char *, const char *);
+char		*strtok_r(char *, const char *, char **);
 
-size_t  strcspn (const char *, const char *);
-size_t  strspn (const char *, const char *);
+size_t		 strcspn(const char *, const char *);
+size_t		 strspn(const char *, const char *);
 
-char *strerror (int);
-const char *syserrlst (int);
+char		*strerror(int);
+const char	*syserrlst(int);

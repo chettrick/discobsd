994062c325279f71e9b557263e48653cb50a2af1
Chris Hettrick <git@structfoo.com>
2025-11-24 04:11:21 -0700

    libc: gen/bzero.c; ANSI, size_t length. Update manpage.
    Update the function prototype for bzero() to:
    
    void bzero(void *b, size_t length);
    
    The length parameter is updated to a size_t.
    
    De-register and ANSI function cleanup.
    
    Update bzero(3) function prototype in bstring(3) manual page.
    Small typo and whitespace fixes as well. Bump the modified date.
    
    Added back the missing 2.11BSD SCCS version tag to bzero.c.

--- include/strings.h
+++ include/strings.h
@@ -40,7 +40,7 @@ typedef	unsigned int	size_t;
 
 int	 bcmp(const void *, const void *, size_t);
 void	 bcopy(const void *, void *, size_t);
-void	 bzero(void *, unsigned long);
+void	 bzero(void *, size_t);
 int	 ffs(int);
 char	*index(const char *, int);
 char	*rindex(const char *, int);
--- lib/libc/gen/bzero.c
+++ lib/libc/gen/bzero.c
@@ -2,15 +2,17 @@
  * Copyright (c) 1980 Regents of the University of California.
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
+ *
+ *	@(#)bzero.c	1.1 (Berkeley) 1/19/87
  */
+
+#include <string.h>
 
 /*
  * bzero -- vax movc5 instruction
  */
 void
-bzero(b, length)
-	register void *b;
-	register unsigned int length;
+bzero(void *b, size_t length)
 {
 	char *p;
 

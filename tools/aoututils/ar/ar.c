chettrick/discobsd

commit 8a46198fdbe8f4b1abd37a1b29e25d8bddf8bf82

usr.bin/ar: Fix possible memory overflow via strlcpy size.
Bug introduced during strcpy(3) -> strlcpy(3) porting in the
destination buffer size.

Pull correct fix from OpenBSD's Attic by using a calculated length
for both malloc(3) and strlcpy(3). Code is cleaner, too.

Keep the tools/aoututils/ar version in sync with this version.

This commit resolves #25 issue for memory overflow in ar(1).

tools/aoututils/ar/ar.c
@@ -122,12 +122,15 @@ main(int argc, char *argv[])
 	 * Fix it, if necessary.
 	*/
 	if (*argv[1] != '-') {
-		if (!(p = malloc((unsigned)(strlen(argv[1]) + 2)))) {
+		size_t len;
+
+		len = (u_int)(strlen(argv[1]) + 2);
+		if (!(p = malloc(len))) {
 			(void)fprintf(stderr, "ar: %s.\n", strerror(errno));
 			exit(1);
 		}
 		*p = '-';
-		(void)strlcpy(p + 1, argv[1], sizeof(p - 1));
+		(void)strlcpy(p + 1, argv[1], len - 1);
 		argv[1] = p;
 	}
 

usr.bin/ar/ar.c
@@ -122,12 +122,15 @@ main(int argc, char *argv[])
 	 * Fix it, if necessary.
 	*/
 	if (*argv[1] != '-') {
-		if (!(p = malloc((unsigned)(strlen(argv[1]) + 2)))) {
+		size_t len;
+
+		len = (u_int)(strlen(argv[1]) + 2);
+		if (!(p = malloc(len))) {
 			(void)fprintf(stderr, "ar: %s.\n", strerror(errno));
 			exit(1);
 		}
 		*p = '-';
-		(void)strlcpy(p + 1, argv[1], sizeof(p - 1));
+		(void)strlcpy(p + 1, argv[1], len - 1);
 		argv[1] = p;
 	}
 

commit 0203d955477d011ae0e807a24757fc136d3b0324

tools/aoututils/ar: Include sys/types.h for u_int.
FreeBSD explicitly needs sys/types.h included for u_int in ar.c.

Commit 8a46198fdbe8f4b1abd37a1b29e25d8bddf8bf82 introduced a build
failure on FreeBSD, and this fixes it. No binary change.

Keep the usr.bin/ar version in sync with this version.

tools/aoututils/ar/ar.c
@@ -34,6 +34,7 @@
  * SUCH DAMAGE.
  */
 #ifdef CROSS
+#   include <sys/types.h>
 #   include <stdint.h>
 #   include <stdio.h>
 #   include <string.h>

usr.bin/ar/ar.c
@@ -34,6 +34,7 @@
  * SUCH DAMAGE.
  */
 #ifdef CROSS
+#   include <sys/types.h>
 #   include <stdint.h>
 #   include <stdio.h>
 #   include <string.h>
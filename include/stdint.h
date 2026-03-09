27861673ec5116fa08713a3e97540b4ee753599e
Chris Hettrick <git@structfoo.com>
2026-03-08 22:46:44 -0600

    sys: Move stdint.h to sys/sys/, make include/ symlink.
    Following all other BSDs, stdint.h is defined in sys/sys/stdint.h
    and usr/include/stdint.h is a symbolic link to sys/sys/stdint.h.
    
    Create the symlink in ${DESTDIR} via include/Makefile, as well as
    update the fsutil base/mi spec for stdint.h and the symlink to it.

--- include/stdint.h
+++ include/stdint.h
@@ -1,17 +1,1 @@
-#ifndef _STDINT_H
-#define _STDINT_H
-
-typedef signed char         int8_t;
-typedef short int           int16_t;
-typedef int                 int32_t;
-typedef long long           int64_t;
-
-typedef unsigned char       uint8_t;
-typedef unsigned short int  uint16_t;
-typedef unsigned int        uint32_t;
-typedef unsigned long long  uint64_t;
-
-#define UINT32_C(x)         (x##U)
-#define UINT64_C(x)         (x##ULL)
-
-#endif /* _STDINT_H */
+sys/stdint.h
